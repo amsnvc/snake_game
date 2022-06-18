618c6d95510b689a539100ab5f1cdeba79b11833
amsnvc <mesinovicamela@gmail.com>
2022-06-18 14:33:33 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import time
+import pygame
+from pygame.locals import *
+
+if __name__ == "__main__":
+    pygame.init()
+
+    surface = pygame.display.set_mode((500, 500))
+    surface.fill((255, 255, 255))
+    pygame.display.flip()
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+            elif event.type == QUIT:
+                running = False
+    time.sleep(5)
